9bae07a2db285fc032b99354181fcd0e767243ec
Chris Walker <chris@thechriswalker.net>
2018-01-26 12:07:25 +0000

    Fixed webpack/npm output.
    webpack was not exposing a library.
    npm was ignoring the webpack'd bundle.

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        libraryTarget: "commonjs2"
+    }
+}
