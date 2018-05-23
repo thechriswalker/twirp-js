c44174c48ab596406775af56f8bf84c68c53fbda
Ian Ha <ianha0@gmail.com>
2018-05-23 14:37:18 -0400

    Remove node-fetch check

--- src/index-node.js
+++ src/index-node.js
@@ -2,15 +2,7 @@
  * TWIRP RPC Client for Node JS
  */
 var client = require("./client");
-
-// this dependency is a peer-dependency, it must be installed
-// but is not necessary for the browser version, so not a hard dependency
-// if installed it must be >= 2.0.0 which has the .buffer function.
 var fetch = require("node-fetch");
-
-if ("buffer" in fetch.Response.prototype === false) {
-    throw new TypeError("node-fetch must be version >= 2.0.0");
-}
 
 const serialize = msg => Buffer.from(msg.serializeBinary());
 const deserialize = responseType => res => res.buffer()
