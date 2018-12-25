b023c4130f47f91deef89a45dcbc0e0f32444000
Michael.Shieh <michael@mddworld.com>
2018-12-25 09:32:38 +0800

    fix 'res.Buffer is not a function'

--- src/index-node.js
+++ src/index-node.js
@@ -5,7 +5,7 @@ var client = require("./client");
 var fetch = require("node-fetch");
 
 const serialize = msg => Buffer.from(msg.serializeBinary());
-const deserialize = responseType => res => res.buffer()
+const deserialize = responseType => res => res.arrayBuffer()
     .then(b => responseType.deserializeBinary(new Uint8Array(b)).toObject());
 
 module.exports = client.clientFactory(fetch, serialize, deserialize);
